fc12d3f77bdc5985aa9958ddb691d5f80657333c
Toufiq 123 <safaeathasantoufiq@gmail.com>
2026-03-26 18:17:47 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -22,6 +22,7 @@ const SelectTrigger = React.forwardRef<
       "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
+    suppressHydrationWarning
     {...props}
   >
     {children}
@@ -144,7 +145,7 @@ const SelectSeparator = React.forwardRef<
     {...props}
   />
 ))
-SelectSeparator.displayName = SelectPrimitive.Separator.displayName
+SelectSeparator.displayName = SeparatorPrimitive.Separator.displayName
 
 export {
   Select,
